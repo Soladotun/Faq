52dd7ce2f2deb7e4d32ba588ed8719f4cc3dc72b
Soladotun Olatunji <soladotuno@gmail.com>
2022-10-14 08:38:41 +0100

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,5 @@
+// Created by Soladotun 
+
+alert('Details Information from https://www.learnhowtobecome.org/computer-careers/web-development/')
+
+alert('Suggestions will helpfull if you want to give any regarding this information !');
